064de2e19cd0d0792002623a753b95fff27a05ff
Amari <jerome.guya@evsu.edu.ph>
2025-02-25 09:24:52 +0800

    changes

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -33,6 +33,10 @@
                     <div class="mt-1 text-sm text-red-600">{{ $message }}</div>
                 @enderror
             </div>
+            <div class="mb-6">
+                {!!htmlFormSnippet()!!}
+                <x-fields_error name="g-recaptcha-response"/>
+            </div>
             <button
                 type="submit"
                 class="w-full px-4 py-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm text-center"
@@ -46,4 +50,3 @@
 </div>
 <x-footer />
 </x-layout>
-
